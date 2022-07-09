fb11b60db85d3eabddc460ed48cbe03c232f7408
Onurlulardan <altuntasonur@outlook.com.tr>
2022-07-09 18:57:46 +0300

    gün2 featured post listelendi

--- MySimpleBlog/Controllers/BlogController.cs
+++ MySimpleBlog/Controllers/BlogController.cs
@@ -24,6 +24,42 @@ namespace MySimpleBlog.Controllers
 
         public PartialViewResult FeaturedPost()
         {
+            //Önerilen Postlar 1
+            var PostTitle1 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 1).Select(y => y.BlogTitle).FirstOrDefault();
+            var PostImage1 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 1).Select(y => y.BlogImage).FirstOrDefault();
+            var PostDate1 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 1).Select(y => y.BlogDate).FirstOrDefault();
+            
+            ViewBag.PostTitle1 = PostTitle1;
+            ViewBag.PostImage1 = PostImage1;
+            ViewBag.PostDate1 = PostDate1;
+
+            //Önerilen Postlar 2
+            var PostTitle2 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 5).Select(y => y.BlogTitle).FirstOrDefault();
+            var PostImage2 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 5).Select(y => y.BlogImage).FirstOrDefault();
+            var PostDate2 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 5).Select(y => y.BlogDate).FirstOrDefault();
+
+            ViewBag.PostTitle2 = PostTitle2;
+            ViewBag.PostImage2 = PostImage2;
+            ViewBag.PostDate2 = PostDate2;
+
+            //Önerilen Postlar 3
+            var PostTitle3 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 3).Select(y => y.BlogTitle).FirstOrDefault();
+            var PostImage3 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 3).Select(y => y.BlogImage).FirstOrDefault();
+            var PostDate3 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 3).Select(y => y.BlogDate).FirstOrDefault();
+
+            ViewBag.PostTitle3 = PostTitle3;
+            ViewBag.PostImage3 = PostImage3;
+            ViewBag.PostDate3 = PostDate3;
+
+            //Önerilen Postlar 4
+            var PostTitle4 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 4).Select(y => y.BlogTitle).FirstOrDefault();
+            var PostImage4 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 4).Select(y => y.BlogImage).FirstOrDefault();
+            var PostDate4 = bm.GetAll().OrderByDescending(z => z.BlogID).Where(x => x.CategoryID == 4).Select(y => y.BlogDate).FirstOrDefault();
+
+            ViewBag.PostTitle4 = PostTitle4;
+            ViewBag.PostImage4 = PostImage4;
+            ViewBag.PostDate4 = PostDate4;
+
             return PartialView();
         }
 
